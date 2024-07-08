c887a3b24defc4ea6c7f46abbd411fc6af7b5897
Miguel Lima <m.angelomdl97@gmail.com>
2024-07-08 16:09:01 -0300

    aula 18 adicionada

---
+++ aula18.py
@@ -0,0 +1,11 @@
+galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+for p in galera:
+    print(f'{p[0]} tem {p[1]} anos de idade.')
+galera2 = []
+dado = []
+for c in range(0, 3):
+    dado.append(str(input('Nome: ')))
+    dado.append(int(input('Idade: ')))
+    galera2.append(dado[:])
+    dado.clear()
+print(galera2)
